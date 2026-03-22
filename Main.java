4160798261b7295db78f18dc3cc6b494c1747006
The_Rectifier <khemantok18@gmail.com>
2026-03-22 16:04:21 +0530

    new changes

---
+++ Main.java
@@ -0,0 +1,65 @@
+import java.util.concurrent.ConcurrentHashMap;
+
+class User {
+    double balance;
+
+    User(double balance) {
+        this.balance = balance;
+    }
+
+    synchronized void credit(double amt) {
+        balance += amt;
+    }
+
+    synchronized boolean debit(double amt) {
+        if (balance < amt) return false;
+        balance -= amt;
+        return true;
+    }
+
+    synchronized double getBalance() {
+        return balance;
+    }
+}
+
+class Wallet {
+    private final ConcurrentHashMap<String, User> map = new ConcurrentHashMap<>();
+
+    void createUser(String id, double bal) {
+        map.putIfAbsent(id, new User(bal));
+    }
+
+    void credit(String id, double amt) {
+        map.get(id).credit(amt);
+    }
+
+    boolean debit(String id, double amt) {
+        return map.get(id).debit(amt);
+    }
+
+    double getBalance(String id) {
+        return map.get(id).getBalance();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Wallet w = new Wallet();
+        w.createUser("u1", 100);
+
+        Thread t1 = new Thread(() ->
+                System.out.println("T1: " + w.debit("u1", 50)));
+
+        Thread t2 = new Thread(() ->
+                System.out.println("T2: " + w.debit("u1", 70)));
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Final: " + w.getBalance("u1"));
+    }
+}
